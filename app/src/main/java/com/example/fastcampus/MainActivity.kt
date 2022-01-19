993ecb68e6c94e4103c8449a663950dd289a626e
HYUN <hyun@HYUNui-MacPro.local>
2022-01-19 19:59:00 +0900

    save

--- app/src/main/java/com/example/fastcampus/MainActivity.kt
+++ app/src/main/java/com/example/fastcampus/MainActivity.kt
@@ -2,10 +2,67 @@ package com.example.fastcampus
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.linearlayout)
+
+        setContentView(R.layout.activity_main)
+        // 한번만 하면 되는 작업
+        Log.d("LifeCycle", "onCreate")
+
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+
+        }
+
+        button.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+
+    }
+
+    override fun onResume() {
+        Log.d("LifeCycle", "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d("LifeCycle", "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d("LifeCycle", "onStop")
+        super.onStop()
+    }
+
+
+    override fun onStart() {
+        Log.d("LifeCycle", "onStart")
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        Log.d("LifeCycle", "onDestroy")
+        super.onDestroy()
+    }
+
+    override fun onRestart() {
+        Log.d("LifeCycle", "onRestart")
+        super.onRestart()
     }
 }
+
+
+
+
