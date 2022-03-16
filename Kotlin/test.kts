2e6e875a3d0c1a363bafcd80c316d9c26273d103
HYUN <hyun@HYUNui-MacPro.local>
2022-03-16 15:38:18 +0900

    save

--- Kotlin/test.kts
+++ Kotlin/test.kts
@@ -1,28 +1,8 @@
-class Person(var name: String? = null, var age: Int? = null) {}
 
-// apply
-// - 적용하다
-// - 객체를 초기화할때 사용하면 좋다
-val gildong = Person().apply {
-    println(name)
-    name = "길동"
-    age = 19
-}
+val number4: Int = 5
 
-// also
-// - 또한
-// - 유효성 검사할 때 좋다
-// - 수신된 객체의 속성을 변경하지 않고 사용할 때
-val gildong2 = Person("victor").also {
-//    isNameisGildong(it.name)
-}
-
-
-// run
-// - 중복 사용을 방지 해준다
-val gildong3: Person = Person().run {
-
-}
-with(gildong3) {
-
+when (number4) {
+    4 -> println("number is 4")
+    "안녕하세요" -> println("hello")
+    is Boolean -> println("boolean")
 }
